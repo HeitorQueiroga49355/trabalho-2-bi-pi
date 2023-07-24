9f66de8e9a621142dd58904280d26af1a9384901
Heitor Queiroga Duarte <heitorqueiroga1@hotmail.com>
2023-07-24 15:45:52 -0300

    página inicial de dashboad de administrador

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -24,14 +8,14 @@ from marca.views import marca_criar,marca_listar,marca_editar,marca_remover
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adm/', crud, name="crud"),
-    path('',index,name='index'),
-    path('produto/',produto_criar,name='produto_criar'),
+    path('adm/produto/criar',produto_criar,name='produto_criar'),
+    path('adm/produto/editar/<int:id>/',produto_editar, name='produto_editar'),
+    path('adm/produto/remover/<int:id>/',produto_remover,name='produto_remover'),
+    path('adm/produto/listar',produto_listar,name='produto_listar'),
+    path('adm/marca/criar',marca_criar,name='marca_criar'),
+    path('adm/marca/editar/<int:id>/',marca_editar, name='marca_editar'),
+    path('amd/marca/remover/<int:id>/',marca_remover,name='marca_remover'),
+    path('',index,name='home'),
     path('produto/<int:id>/',produto_detalhe,name='produto_detalhe'),
-    path('produto/editar/<int:id>/',produto_editar, name='produto_editar'),
-    path('produto/remover/<int:id>/',produto_remover,name='produto_remover'),
-    path('produto/listar',produto_listar,name='produto_listar'),
-    path('marca/',marca_criar,name='marca_criar'),
-    path('marca/editar/<int:id>/',marca_editar, name='marca_editar'),
-    path('marca/remover/<int:id>/',marca_remover,name='marca_remover'),
     path('marca/listar',marca_listar,name='marca_listar'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
